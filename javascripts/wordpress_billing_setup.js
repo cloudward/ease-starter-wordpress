1fd172128f95f96ee8b30ce958a5b246488c7bd6
Lucas <loomtronic@gmail.com>
2014-05-30 22:08:21 -0700

    Committing missing WP JS files
    These files show/hide the Merchant App Link at different times
    
    https://app.asana.com/0/11562549203986/12743813922320

---
+++ javascripts/wordpress_billing_setup.js
@@ -0,0 +1,4 @@
+jQuery( document ).ready(function() {
+	jQuery('a[href$="admin.php?page=ease_merchant_application_demo"]:first').html('EASE Merchant Application');
+	jQuery('a[href$="admin.php?page=ease_merchant_application_demo"]:first').attr('href','admin.php?page=ease_merchant_application&edit=04df66fee0644a1bb070b730fab29f6d');
+});
---
+++ javascripts/wordpress_billing_setup_hide.js
@@ -0,0 +1,5 @@
+jQuery( document ).ready(function() {
+	jQuery('a[href$="admin.php?page=ease_merchant_application_demo"]:first').html('EASE Billing Setup');
+	jQuery('a[href$="admin.php?page=ease_merchant_application_demo"]:first').attr('href','admin.php?page=ease_admin_billing_setup');
+
+});
